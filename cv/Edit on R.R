225e5b446ffd718c8fb69779c6208aabd6cbc2a4
Bruno T. Tomio <44265732+bttomio@users.noreply.github.com>
2022-02-20 10:58:13 +0100

    cv ok

--- cv/Edit on R.R
+++ cv/Edit on R.R
@@ -8,12 +8,14 @@ cv_entries <- cv_entries[c(1:3,4,4:10),]
 
 cv_entries[4,2] <- "Since 1/2022"
 cv_entries[4,3] <- ""
-cv_entries[4,4] <- "PhD Intern"
+cv_entries[4,4] <- "Ph.D. Intern"
 cv_entries[4,5] <- "Bank of England, United Kingdom"
 
 cv_entries[2,4] <- "M.A. in Int. Economics"
 
 cv_entries[5,2] <- "Since 6/2012"
+
+cv_entries[3,4] <- "B.Sc. in Economics"
 
 library(openxlsx)
 write.xlsx(cv_entries, 'data/cv_entries.xlsx',
